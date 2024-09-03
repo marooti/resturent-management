1a526859fe46cc06549a5dcfae40198231c31b2b
Muhammad Imran <88757033+marooti@users.noreply.github.com>
2024-09-03 16:38:11 +0500

    Chnaging

--- src/app/pages/components/dashboard/menu/menu.component.ts
+++ src/app/pages/components/dashboard/menu/menu.component.ts
@@ -8,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+  listOfMenu: any[] = [
+    {
+      image: 'https://s23209.pcdn.co/wp-content/uploads/2022/07/220602_DD_The-Best-Ever-Cheeseburger_267-500x500.jpg',
+      foodName: "Cheese Burger",
+      price: "300"
+    },
+    {
+      image: 'https://s23209.pcdn.co/wp-content/uploads/2022/07/220602_DD_The-Best-Ever-Cheeseburger_267-500x500.jpg',
+      foodName: "Chicken Burger",
+      price: "370"
+    },
+    {
+      image: 'https://s23209.pcdn.co/wp-content/uploads/2022/07/220602_DD_The-Best-Ever-Cheeseburger_267-500x500.jpg',
+      foodName: "Turkish Burger",
+      price: "570"
+    },
+    {
+      image: 'https://s23209.pcdn.co/wp-content/uploads/2022/07/220602_DD_The-Best-Ever-Cheeseburger_267-500x500.jpg',
+      foodName: "Turkish Burger",
+      price: "570"
+    },
+    {
+      image: 'https://s23209.pcdn.co/wp-content/uploads/2022/07/220602_DD_The-Best-Ever-Cheeseburger_267-500x500.jpg',
+      foodName: "Turkish Burger",
+      price: "570"
+    },
+    {
+      image: 'https://s23209.pcdn.co/wp-content/uploads/2022/07/220602_DD_The-Best-Ever-Cheeseburger_267-500x500.jpg',
+      foodName: "Turkish Burger",
+      price: "570"
+    },
 
+  ]
 }
