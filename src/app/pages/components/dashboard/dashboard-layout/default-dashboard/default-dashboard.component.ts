bc196e222f7ab5dcfc5db6103982111da9ec9fd3
umer4153 <umeralidev9@gmail.com>
2024-09-08 21:19:23 +0500

    structure done

---
+++ src/app/pages/components/dashboard/dashboard-layout/default-dashboard/default-dashboard.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { PerformanceChartComponent } from './performance-chart/performance-chart.component';
+import { MenuItem } from 'primeng/api';
+import { TabMenuModule } from 'primeng/tabmenu';
+
+
+@Component({
+  selector: 'app-default-dashboard',
+  standalone: true,
+  imports: [PerformanceChartComponent,TabMenuModule],
+  templateUrl: './default-dashboard.component.html',
+  styleUrl: './default-dashboard.component.scss'
+})
+export class DefaultDashboardComponent implements OnInit{
+  tabItems!: MenuItem[];
+
+  ngOnInit(): void {
+    this.tabMenu();
+  }
+
+  tabMenu() {
+    this.tabItems = [
+      { label: 'Agent Dashboard', routerLink: ['/agent-dashboard'] },
+      { label: 'Customer Feedback', routerLink: ['/dashboard'] },
+      { label: 'Recent Activity', routerLink: ['/dashboard'] }
+    ];
+  }
+
+}
